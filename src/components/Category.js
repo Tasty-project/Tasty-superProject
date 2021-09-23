d04be25d1e4ac0097da927cde0520fcce1f110f0
Chenchenlin789 <chenchenlihn@gmail.com>
2021-09-23 18:03:05 +0200

    rename-update

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css';
+import './Category.css';
 
 class Home extends Component {
     constructor(props) {
